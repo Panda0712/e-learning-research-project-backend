249b347221221c8a3f659216c05818d325e87e37
Pham Tuan <tuan.pham.super@gmail.com>
2026-01-27 13:30:55 +0700

    feature/ptuan: implement email verification with brevo and codebase for resend and mailersend

--- src/generated/prisma/internal/prismaNamespace.ts
+++ src/generated/prisma/internal/prismaNamespace.ts
@@ -2462,6 +2462,7 @@ export const UserScalarFieldEnum = {
   phoneNumber: 'phoneNumber',
   dateOfBirth: 'dateOfBirth',
   role: 'role',
+  verifyToken: 'verifyToken',
   isVerified: 'isVerified',
   createdAt: 'createdAt',
   updatedAt: 'updatedAt',
@@ -2951,7 +2952,8 @@ export const UserOrderByRelevanceFieldEnum = {
   lastName: 'lastName',
   avatar: 'avatar',
   phoneNumber: 'phoneNumber',
-  role: 'role'
+  role: 'role',
+  verifyToken: 'verifyToken'
 } as const
 
 export type UserOrderByRelevanceFieldEnum = (typeof UserOrderByRelevanceFieldEnum)[keyof typeof UserOrderByRelevanceFieldEnum]
