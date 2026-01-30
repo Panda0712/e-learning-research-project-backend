a860522d5a4cfb4db0b8a9a34d2dd29e1fe2e7c3
Minh Khoa <khoaminh010704@gmail.com>
2026-01-30 21:19:21 +0700

    feature/minhkhoa-get-data-dashboard

--- src/routes/v1/dashboardRoute.ts
+++ src/routes/v1/dashboardRoute.ts
@@ -1,4 +1,5 @@
 import { dashboardController } from "@/controllers/dashboardController.js";
+import { verifyToken } from "@/middlewares/authMiddleware.js";
 import express from "express";
 
 const Router = express.Router();
@@ -8,7 +9,7 @@ Router.get("/admin/overview", dashboardController.getOverview);
 Router.get("/admin/charts", dashboardController.getCharts);
 
 // --- Route Lecturer ---
-Router.get("/lecturer/overview", dashboardController.getLecturerOverview);
-Router.get("/lecturer/charts", dashboardController.getLecturerCharts);
+Router.get("/lecturer/overview", verifyToken, dashboardController.getLecturerOverview);
+Router.get("/lecturer/charts", verifyToken, dashboardController.getLecturerCharts);
 
 export const dashboardRoute = Router;
