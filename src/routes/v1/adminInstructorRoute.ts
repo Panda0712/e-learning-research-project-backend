aab69e7589f1d3540d37679265f7dcd9c7dd4f8e
Phạm Ngũ Tuân <123355012+Panda0712@users.noreply.github.com>
2026-03-21 20:55:39 +0700

    Update adminInstructorRoute.ts

--- src/routes/v1/adminInstructorRoute.ts
+++ src/routes/v1/adminInstructorRoute.ts
@@ -4,8 +4,6 @@ import { authMiddleware } from "@/middlewares/authMiddleware.js"; // Bật middl
 
 const Router = express.Router();
 
-// Tạm thời mình khoan gắn authMiddleware.isAdmin để bạn test Frontend cho dễ nhé.
-// Sau này ráp xong thì thêm vào sau.
 Router.route("/").get(adminInstructorController.getAllRequests);
 Router.route("/:id/approve").put(adminInstructorController.approveRequest);
 Router.route("/:id/reject").put(adminInstructorController.rejectRequest);
