08f595948bd4725cf5eb68b9d37328363a23552a
Pham Tuan <pham.tuan@hitachids.com>
2026-03-30 14:03:37 +0700

    feature/ptuan: config and init ioredis client

--- src/utils/ApiError.ts
+++ src/utils/ApiError.ts
@@ -1,3 +1,5 @@
+import { StatusCodes } from "http-status-codes";
+
 class ApiError extends Error {
   public statusCode: number;
 
@@ -9,6 +11,15 @@ class ApiError extends Error {
     this.statusCode = statusCode;
 
     Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export class RedisErrorResponse extends ApiError {
+  constructor(
+    statusCode = StatusCodes.INTERNAL_SERVER_ERROR,
+    message = StatusCodes[statusCode] as string,
+  ) {
+    super(statusCode, message);
   }
 }
 
